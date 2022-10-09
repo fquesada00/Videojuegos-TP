5a6cbeb1a543a87f5b686e932a1955e042f24ecc
octavio serpe <octavio.serpe@gmail.com>
2022-10-09 19:59:05 -0300

    :sparkles: add nav mesh to enemies and make them follow the player

---
+++ Assets/Scripts/Entities/NavMesh/EnemyFollow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Entities.NavMesh
+{
+    public class EnemyFollow : MonoBehaviour
+    {
+        private Actor _player;
+        private NavMeshAgent _navMeshAgent;
+
+        private void Start()
+        {
+            _player = FindObjectOfType<Actor>();
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            _navMeshAgent.SetDestination(_player.transform.position);
+        }
+    }
+}
