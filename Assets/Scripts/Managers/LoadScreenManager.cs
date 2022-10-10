4c7e8d6e1aaacd7aadd716d4c5829d5b5f0c6678
Manuel Joaquín Rodriguez <manurodriguez@itba.edu.ar>
2022-10-10 04:31:49 +0200

    :sparkles: add loading scene

---
+++ Assets/Scripts/Managers/LoadScreenManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScreenManager : MonoBehaviour
+{
+    [SerializeField] private Image _progressBar;
+    [SerializeField] private Text _progressValue;
+    [SerializeField] private string _targetScene = "Level"; // podria ser un parametro
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsync());
+    }
+
+    IEnumerator LoadAsync()
+    {
+        // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+        AsyncOperation async = SceneManager.LoadSceneAsync(_targetScene);
+
+        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+        while (!async.isDone)
+        {
+            _progressBar.fillAmount = async.progress;
+            _progressValue.text = $"LOADING - {async.progress * 100}%";
+            yield return null;
+        }
+    }
+}
