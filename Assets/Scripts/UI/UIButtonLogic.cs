234f1b499ae37a69abfa0124bd8e7a2b0dcaf183
Manuel Joaquín Rodriguez <manurodriguez@itba.edu.ar>
2022-10-10 05:20:07 +0200

    :sparkles: add information dialog

--- Assets/Scripts/UI/UIButtonLogic.cs
+++ Assets/Scripts/UI/UIButtonLogic.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
+[RequireComponent(typeof(GameObject))]
 public class UIButtonLogic : MonoBehaviour
 {
+    public GameObject InformationDialog => _informationDialog;
+    [SerializeField] private GameObject _informationDialog;
+    
     public void LoadMenuScene() => SceneManager.LoadScene("Menu");
     public void LoadLevelScene() => SceneManager.LoadScene("LoadingScene");
-    public void LoadSettingsScene() => Debug.Log("Settings scene");
+    public void ToggleInformationDialog() => _informationDialog.SetActive(!_informationDialog.activeSelf);
     public void CloseGame() => Application.Quit();
 }
