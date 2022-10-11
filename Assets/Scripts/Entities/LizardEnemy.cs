a0aff66454e5825f07f16d8f7a09640dbc5f21ac
octavio serpe <octavio.serpe@gmail.com>
2022-10-10 21:20:36 -0300

    :construction: add lizard enemy

--- Assets/Scripts/Entities/LizardEnemy.cs
+++ Assets/Scripts/Entities/LizardEnemy.cs
@@ -2,24 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Controllers;
+using Controllers.Utils;
 
 [RequireComponent(typeof(LifeController))]
 public class LizardEnemy : Enemy
 {
-    // Start is called before the first frame update
+    private Animator _animator;
+
+    private Cooldown _attackCooldown;
+    private static readonly int AttackId = Animator.StringToHash("attack");
+
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        _attackCooldown = new Cooldown();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Attack();
     }
 
     public override void Attack()
     {
-           
+        if (_attackCooldown.IsOnCooldown()) return;
+        _animator.SetTrigger(AttackId);
+        StartCoroutine(_attackCooldown.BooleanCooldown(this.EnemyStats.AttackCooldown));
     }
 }
---
+++ Assets/Scripts/Entities/PatrolEnemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Entities
+{
+    public class PatrolEnemy : MonoBehaviour
+    {
+
+        public Transform[] waypoints;
+        private int _currentWaypointIndex = 0;
+        private float _speed = 2f;
+
+        private void Update()
+        {
+            Transform wp = waypoints[_currentWaypointIndex];
+            if (Vector3.Distance(transform.position, wp.position) < 0.01f)
+            {
+                _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
+            }
+            else
+            {
+                var position = wp.position;
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    position,
+                    _speed * Time.deltaTime);
+                transform.LookAt(position);
+            }
+        }
+
+    }
+}
