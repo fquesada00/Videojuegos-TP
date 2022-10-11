e28516742a0f56785beb13b5c6a0b0276b33932c
octavio serpe <octavio.serpe@gmail.com>
2022-10-10 22:58:21 -0300

    :sparkles: add lizard enemy controller with patrol enemy
    Co-authored-by: Agustin Jerusalinsky <ajerusalinsky@itba.edu.ar>

--- Assets/Scripts/Entities/LizardEnemy.cs
+++ Assets/Scripts/Entities/LizardEnemy.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using Controllers;
 using Controllers.Utils;
+using Entities;
 
 [RequireComponent(typeof(LifeController))]
-public class LizardEnemy : Enemy
+public class LizardEnemy : PatrolEnemy
 {
     private Animator _animator;
 
     private Cooldown _attackCooldown;
     private static readonly int AttackId = Animator.StringToHash("attack");
+    private static readonly int IdleId = Animator.StringToHash("idle");
+    private static readonly int MoveId = Animator.StringToHash("move");
 
-    void Start()
+
+    private PatrolEnemy _patrolEnemy;
+    
+    void OnEnable()
     {
+        base.OnEnable();
         _animator = GetComponent<Animator>();
         _attackCooldown = new Cooldown();
+        
+        _patrolEnemy = GetComponent<PatrolEnemy>();
     }
 
     void Update()
     {
-        Attack();
+        _animator.SetFloat("speed", NavMeshAgent.velocity.magnitude);
+        base.Update();
+        if(_patrolEnemy.IsOnEnemyRange())
+        {
+            var position = _player.transform.position;
+            transform.LookAt(new Vector3(position.x, transform.position.y, position.z));
+            Attack();
+        }
     }
 
     public override void Attack()
