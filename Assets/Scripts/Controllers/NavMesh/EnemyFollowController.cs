bf7be14880b30b4045a622e3331362381a85e275
octavio serpe <octavio.serpe@gmail.com>
2022-11-14 00:39:28 -0300

    :sparkles: add crab explosion range and animation with damage to player

--- Assets/Scripts/Controllers/NavMesh/EnemyFollowController.cs
+++ Assets/Scripts/Controllers/NavMesh/EnemyFollowController.cs
@@ -9,15 +9,25 @@ namespace Controllers.NavMesh
         public NavMeshAgent NavMeshAgent => _navMeshAgent;
         private Actor _player;
         private NavMeshAgent _navMeshAgent;
+        
+        private bool _moveToPlayer;
+        public bool MoveToPlayer
+        {
+            set => _moveToPlayer = value;
+            get => _moveToPlayer;
+        }
 
         private void Awake()
         {
             _player = FindObjectOfType<Actor>();
             _navMeshAgent = GetComponent<NavMeshAgent>();
+            _moveToPlayer = true;
         }
 
         private void Update()
         {
+            if(!_moveToPlayer) return;
+            
             _navMeshAgent.SetDestination(_player.transform.position);
             //look at player y axis
             transform.LookAt(new Vector3(_player.transform.position.x, transform.position.y, _player.transform.position.z));
---
+++ Assets/Scripts/Entities/CrabEnemy.cs
@@ -0,0 +1,64 @@
+﻿using Controllers;
+using Controllers.NavMesh;
+using Controllers.Utils;
+using UnityEngine;
+
+namespace Entities
+{
+    [RequireComponent(typeof(LifeController))]
+    public class CrabEnemy : Enemy
+    {
+        private Cooldown _attackCooldown = new Cooldown();
+        private bool _isAttacking = false;
+
+        new void OnEnable()
+        {
+            base.OnEnable();
+
+            _enemyFollowController = GetComponent<EnemyFollowController>();
+
+            base.SoundController = GetComponent<SoundController>();
+            
+            // TODO: DIE SOUND and maybe a WALKING ONE?
+        }
+
+        void Update()
+        {
+            // if enemy is attacking, do nothing as it's exploding
+            if (_isAttacking)
+                _enemyFollowController.MoveToPlayer = false;
+
+            float distanceFromPlayer = _enemyFollowController.getDistanceFromPlayer();
+            
+            if (distanceFromPlayer < 0.5f) // TODO: HARDCODED
+                Attack();
+        }
+
+        public override void Attack()
+        {
+            // add a cooldown to give the player time to react
+            if (_attackCooldown.IsOnCooldown()) return;
+            
+            GetComponent<Animator>().SetTrigger("Sleep");
+            _isAttacking = true;
+            StartCoroutine(_attackCooldown.CallbackCooldown(this.Stats.AttackCooldown, FixedAttack));
+        }
+
+        private void FixedAttack()
+        {
+            // detect if the player is in range
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, this.EnemyStats.AttackRange);
+            foreach (var hitCollider in hitColliders)
+            {
+                if (hitCollider.gameObject.CompareTag("Player"))
+                {
+                    bool hitCrit = Random.Range(0, 1) < this.Stats.CritProbability;
+                    hitCollider.gameObject.GetComponent<LifeController>().TakeDamage(this.Stats.Damage, hitCrit);
+                    break;
+                }
+            }
+
+            Die();
+        }
+    }
+}
