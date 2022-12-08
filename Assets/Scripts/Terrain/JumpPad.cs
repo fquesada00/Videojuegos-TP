d8df45418c03d7ebf95c7d983ca51e944cdd13eb
Agustin Jerusalinsky <ajerusalinsky@itba.edu.ar>
2022-12-08 20:23:06 -0300

    jumpads placed, fix jumpad movement generate lighting
    Co-authored-by: Manuel Rodriguez <manurodriguez@itba.edu.ar>

--- Assets/Scripts/Terrain/JumpPad.cs
+++ Assets/Scripts/Terrain/JumpPad.cs
@@ -4,7 +4,9 @@ public class JumpPad : MonoBehaviour
 {
     const float _horizontalSpeed = 30;
     Transform _target;
+    Transform _origin;
     Vector3 _initialSpeed;
+    Vector3 _speed;
     float _jumpTotalTime, _jumpTime;
     bool _isJumping = false;
     GameObject _jumper;
@@ -12,27 +14,27 @@ public class JumpPad : MonoBehaviour
     void Start()
     {
         _target = transform.Find("Target");
+        _origin = transform.Find("Origin");
         Vector3 direction = _target.position - transform.position;
         float range = Vector3.Distance(transform.position, _target.position);
         _jumpTotalTime = range / _horizontalSpeed;
         float deltaY = _target.position.y - transform.position.y;
         float _ySpeed = (deltaY - 0.5f * Physics.gravity.y * Mathf.Pow(_jumpTotalTime, 2) ) / _jumpTotalTime;
-        
         _initialSpeed = _ySpeed * Vector3.up + _horizontalSpeed * direction.normalized;
+
+        //align up direction with initial speed direction
+        _origin.rotation = Quaternion.LookRotation(_initialSpeed);
+
     }
 
-    void Update()
+    void LateUpdate()
     {
         if (_isJumping) {
+            float dt = Time.time - _jumpTime;
+            _jumper.transform.position = _origin.position + _initialSpeed * (float)dt + 0.5f * Physics.gravity * (float)Mathf.Pow(dt, 2);
 
-            _initialSpeed += Physics.gravity * Time.deltaTime;
-            _jumper.transform.position += _initialSpeed * Time.deltaTime;
-            _jumpTime += Time.deltaTime;
-
-            if(_jumpTime >= _jumpTotalTime - 0.4f) {
+            if(dt >= _jumpTotalTime - 0.4f) {    
                 _isJumping = false;
-                // FIXME: should be on Queue
-                _jumper.GetComponent<MovementController>().enabled = true;
             }
         }
     }
@@ -41,8 +43,8 @@ public class JumpPad : MonoBehaviour
         if (other.gameObject.CompareTag("Player")) {
             _isJumping = true;
             _jumper = other.gameObject;
-            _jumper.GetComponent<MovementController>().enabled = false;
-            _jumpTime = 0;
+            _jumpTime = Time.time;
+            _speed = _initialSpeed;
         }
     }
 }
