c295ef807f018a1e66f07d3682d60884a0de8811
Francisco Quesada <fquesada@itba.edu.ar>
2022-12-16 13:56:35 -0300

    add sounds to end scene

--- Assets/Scripts/Managers/EndGameManager.cs
+++ Assets/Scripts/Managers/EndGameManager.cs
@@ -10,10 +10,15 @@ public class EndGameManager : MonoBehaviour
     [SerializeField] private Sprite _victorySprite;
     [SerializeField] private Sprite _defeatSprite;
 
+    [SerializeField] private AudioClip winSound;
+    [SerializeField] private AudioClip defeatSound;
+
+
     private void Start()
     {
         _endStatusImage.sprite = GlobalDataManager.Instance.IsVictory ? _victorySprite : _defeatSprite;
         _endStatusText.text = GlobalDataManager.Instance.IsVictory ? "Victory!" : "Defeat!";
         _endStatusText.color = GlobalDataManager.Instance.IsVictory ? Color.yellow : Color.red;
+        Camera.main.GetComponent<AudioSource>().PlayOneShot(GlobalDataManager.Instance.IsVictory ? winSound : defeatSound, AudioListener.volume);
     }
 }
